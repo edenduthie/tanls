ea3b75fc16aa78959faaa1af0ed366bd7c5f700c
edenduthie <eduthie@gmail.com>
2012-07-09 06:27:46 +0000

    MailServiceTest.java

---
+++ test/tanls/service/MailServiceTest.java
@@ -0,0 +1,27 @@
+package tanls.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import tanls.BaseTest;
+
+public class MailServiceTest extends BaseTest
+{
+    @Autowired MailService mailService;
+    
+    @Test
+    public void checkConfig()
+    {
+    	Assert.assertNotNull(mailService.getSmtpServer());
+    }
+    
+    @Test
+    public void sendMessage() throws AddressException, MessagingException
+    {
+    	mailService.sendMessage("eduthie@gmail.com","Subject","Content");
+    }
+}
