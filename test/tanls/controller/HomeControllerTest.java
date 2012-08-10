3274723cff188911b14ea088aa5f8da94eed4e0e
edenduthie <eduthie@gmail.com>
2012-08-10 06:33:44 +0000

    HomeControllerTest.java

---
+++ test/tanls/controller/HomeControllerTest.java
@@ -0,0 +1,10 @@
+package tanls.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tanls.BaseTest;
+
+public class HomeControllerTest extends BaseTest
+{
+	@Autowired HomeController homeController;
+}
