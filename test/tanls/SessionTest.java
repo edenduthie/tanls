2388b6ff0c6337838fbd12284d1d31ee10201618
edenduthie <eduthie@gmail.com>
2012-08-15 06:31:17 +0000

    SessionTest.java

---
+++ test/tanls/SessionTest.java
@@ -0,0 +1,14 @@
+package tanls;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration(locations={"file:test/mvc-dispatcher-servlet.xml"}) 
+@TestExecutionListeners({ 
+    WebContextTestExecutionListener.class, 
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class })
+public class SessionTest extends AbstractTestNGSpringContextTests {}
