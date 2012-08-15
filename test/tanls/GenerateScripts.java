0c36f1714696bcd9b3ab9df2bd0e292d93497f96
edenduthie <eduthie@gmail.com>
2012-08-15 06:31:43 +0000

    GenerateScripts.java

---
+++ test/tanls/GenerateScripts.java
@@ -0,0 +1,44 @@
+package tanls;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tanls.database.SuburbDAO;
+import tanls.entity.Suburb;
+
+public class GenerateScripts extends BaseTest
+{
+	String suburbsFile = "scripts/suburbs.sql";
+	
+	@Autowired SuburbDAO suburbDAO;
+	
+    //@Test
+    public void generateSuburbs() throws IOException
+    {
+    	List<Suburb> suburbs = suburbDAO.getAll();
+    	
+    	FileWriter writer = new FileWriter(new File(suburbsFile));
+    	
+    	writer.write("insert into suburb(id,name) values");
+    	
+    	
+    	int i=0;
+    	
+    	for( Suburb suburb : suburbs )
+    	{
+    		String values = "";
+    		if( i++ > 0 ) values += ",";
+    		values += "(" + suburb.getId() + ",'" + suburb.getName() + "')";
+    		writer.write(values);
+    	}
+    	
+    	writer.write(";");
+    	
+    	writer.flush();
+    	writer.close();
+    }
+}
