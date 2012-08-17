6a6c1e30ca149bcd60894df0f9d07812c148a679
edenduthie <eduthie@gmail.com>
2012-08-17 06:30:59 +0000

    BaseTest.java

---
+++ test/tanls/BaseTest.java
@@ -0,0 +1,7 @@
+package tanls;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration(locations={"file:test/mvc-dispatcher-servlet.xml"})
+public class BaseTest extends AbstractTestNGSpringContextTests {}
