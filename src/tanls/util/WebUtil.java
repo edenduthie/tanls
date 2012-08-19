eab0a86d6db0f02cc46da339558c0c8782af3aef
edenduthie <eduthie@gmail.com>
2012-08-19 06:30:29 +0000

    WebUtil.java

---
+++ src/tanls/util/WebUtil.java
@@ -0,0 +1,48 @@
+package tanls.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.util.HtmlUtils;
+
+public class WebUtil 
+{
+	public static String getIpAddress(HttpServletRequest request)
+	{
+		String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("WL-Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_CLIENT_IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getRemoteAddr();  
+        }  
+        return ip;
+	}
+	
+    public static String nl2lb(String text)
+    {
+		String newText = HtmlUtils.htmlEscape(text);
+		newText = newText.replaceAll("(\r\n|\n\r|\r|\n)", "<br />");
+		return newText;
+    }
+    
+    public static boolean isANumber(String name) 
+    {
+    	try
+    	{
+    	    Long nameAsLong = Long.parseLong(name);
+    	    return true;
+    	}
+    	catch(NumberFormatException e ) {
+    		return false;
+    	}
+    }
+}
