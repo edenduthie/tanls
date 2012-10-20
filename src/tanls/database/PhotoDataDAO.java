2756ffb88f732b84b6f5e85b884bd45905808da8
edenduthie <eduthie@gmail.com>
2012-10-20 06:27:12 +0000

    PhotoDataDAO.java

---
+++ src/tanls/database/PhotoDataDAO.java
@@ -0,0 +1,39 @@
+package tanls.database;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tanls.entity.PhotoData;
+
+@Service
+public class PhotoDataDAO 
+{
+	@Autowired Database db;
+	
+    public void put(PhotoData photoData)
+    {
+    	db.persist(photoData);
+    }
+    
+    public PhotoData get(Integer id)
+    {
+    	return (PhotoData) db.getNoNull(PhotoData.class.getName(), "id", id);
+    }
+    
+    public void update(PhotoData photoData)
+    {
+    	db.update(photoData);
+    }
+    
+    public void deleteAll()
+    {
+    	db.deleteAll(PhotoData.class);
+    }
+
+	public List<PhotoData> getAll() 
+	{
+		return db.getAll(PhotoData.class.getName());
+	}
+}
